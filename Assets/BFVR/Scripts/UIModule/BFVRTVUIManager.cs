aed28806bef84425952a6681c8e95e427cb9ed76
DevGuyBlake <bmartin@dmi.edu>
2022-02-21 12:45:33 -0600

    Set up video player prefab.

--- Assets/BFVR/Scripts/UIModule/BFVRTVUIManager.cs
+++ Assets/BFVR/Scripts/UIModule/BFVRTVUIManager.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
 namespace BFVR.UIModule
 {
     public class BFVRTVUIManager : MonoBehaviour
     {
         #region TV Interface Prompt
-        public GameObject TVScreenMesh;
 
         [Space]
         public GameObject ChapterSelect;
@@ -19,6 +20,11 @@ namespace BFVR.UIModule
         public GameObject WatchSectionCompletePrompt;
         public GameObject InteractiveSectionBeginPrompt;
         public GameObject InteractiveSectionCompletePrompt;
+
+        [Space]
+        [Header("VideoPlayer")]
+        public GameObject TVScreenMesh;
+        public VideoPlayer vidPlayer;
         #endregion
 
         private void Start()
@@ -60,6 +66,7 @@ namespace BFVR.UIModule
             if (DirectionsPromt) DirectionsPromt.SetActive(true);
         }
 
+
         public void DisplaySectionCompletePrompt()
         {
             HidePrompts();
@@ -76,6 +83,23 @@ namespace BFVR.UIModule
         {
             HidePrompts();
             if (InteractiveSectionCompletePrompt) InteractiveSectionCompletePrompt.SetActive(true);
+        }
+
+
+        ////Video Player functionality////
+        public void PlayVideo(VideoClip v)
+        {
+            vidPlayer.clip = v;
+            vidPlayer.Play();
+        }
+        public void PauseVideo()
+        {
+            vidPlayer.Pause();//unsure if play after pause resumes playback because there is no available unpause method. Should be fine. If video restarts, disable setting clip again above.
+        }
+        public void StopVideo()
+        {
+            vidPlayer.Stop();
+            vidPlayer.clip = null;//video player sometimes won't load a new video clip before playing again if the last one stopped before it was finished. This prevents that by unloading the previous videoclip.
         }
     }
 }
