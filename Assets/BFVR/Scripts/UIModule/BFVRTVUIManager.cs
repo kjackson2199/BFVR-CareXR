f481483bd77f63f9e00991c60ef5123894ef1a4c
DevGuyBlake <bmartin@dmi.edu>
2022-02-25 11:37:02 -0600

    Added implementation of media panel appear/disappear. Changed mediapanel object active by default in all scenes.

--- Assets/BFVR/Scripts/UIModule/BFVRTVUIManager.cs
+++ Assets/BFVR/Scripts/UIModule/BFVRTVUIManager.cs
@@ -64,12 +64,14 @@ namespace BFVR.UIModule
         public void DisplayMediaControlPanel()
         {
             HidePrompts();
-            if (MediaControlPanel) MediaControlPanel.SetActive(true);
+            mediaPanelAnim.SetTrigger("up");
+            //if (MediaControlPanel) MediaControlPanel.SetActive(true);
         }
 
         public void HideMediaPanel()
         {
-            if (MediaControlPanel) MediaControlPanel.SetActive(false);
+            mediaPanelAnim.SetTrigger("down");
+            //if (MediaControlPanel) MediaControlPanel.SetActive(false);
         }
 
         public void DisplaySectionBeginPrompt()
