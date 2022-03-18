42027e900f626313c0b1f5e9631394a81718c848
Jess Salas <jesssalas1995@gmail.com>
2022-03-18 17:02:15 -0500

    Disabled SP buttons when watching and interacting

--- Assets/BFVR/Scripts/ChapterManagement/BFVRChapterSectionManager.cs
+++ Assets/BFVR/Scripts/ChapterManagement/BFVRChapterSectionManager.cs
@@ -8,19 +8,28 @@ public class BFVRChapterSectionManager : MonoBehaviour
     public delegate void OnSectionBeginDelegate();
     //public delegate void OnSectionCompleteDelegate();
     public delegate void OnSectionResetDelegate();
+    public delegate void OnSectionEndDelegate();
 
     public static event OnSectionBeginDelegate onSectionBeginEvent;
     //public static event OnSectionCompleteDelegate onSectionCompleteEvent;
     public static event OnSectionResetDelegate onSectionResetEvent;
+    public static event OnSectionEndDelegate onSectionEndEvent;
 
     public UnityEvent OnSectionBegin;
     //public UnityEvent OnSectionCompleteEvent;
     public UnityEvent OnSectionReset;
+    public UnityEvent OnSectionEnd;
 
     public virtual void BeginSection()
     {
         if (onSectionBeginEvent != null) onSectionBeginEvent.Invoke();
         if (OnSectionBegin != null) OnSectionBegin.Invoke();
+    }
+
+    public virtual void EndSection()
+    {
+        if (onSectionEndEvent != null) onSectionEndEvent.Invoke();
+        if (OnSectionEnd != null) OnSectionEnd.Invoke();
     }
 
     public virtual void ResetSection()
