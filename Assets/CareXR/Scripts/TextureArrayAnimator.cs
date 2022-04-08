de072e277beca45472ce03f42fd52a3b06dd4f8a
DevGuyBlake <bmartin@dmi.edu>
2022-04-08 16:25:36 -0500

    Finished tube liquid animations and animation function can now be called from stepmanager.

--- Assets/CareXR/Scripts/TextureArrayAnimator.cs
+++ Assets/CareXR/Scripts/TextureArrayAnimator.cs
@@ -4,19 +4,61 @@ using UnityEngine;
 
 public class TextureArrayAnimator : MonoBehaviour
 {
-    public Texture[] texList;
+    public Texture[] texAnim1;
+    public Texture[] texAnim2;
+    public Texture[] texAnim3;
+    public Texture[] texAnim4;
+    public Texture[] texAnim5;
     int curTex = 0;
+    int targetAnim = 1;
+    public int targetMat = 0;
 
+    //void Start()
+    //{
+    //    //Add stuff here for testing
+    //    PlayTexAnim(1);
+    //}
 
-    public void PlayTexAnim()
+    public void PlayTexAnim(int animationNumber)
     {
+        targetAnim = animationNumber;
+        curTex = 0;
         StartCoroutine(Switch());
     }
     
-    IEnumerator Switch()
+    IEnumerator Switch()//This amazing script was made by Blake Martin using the help of Unity forum users and outdated Unity Documentation.
     {
-        yield return new WaitForEndOfFrame();//This is why we're not using a loop here.
-        GetComponent<Renderer>().material.mainTexture = texList[curTex];
-        if (curTex < texList.Length) StartCoroutine(Switch());
+        yield return new WaitForEndOfFrame();//This is why we're using a coroutine instead of a loop.
+        if (targetAnim == 1)
+        {
+            Debug.Log(curTex);
+            GetComponent<Renderer>().materials[targetMat].SetTexture("_MainTex", texAnim1[curTex]);
+            curTex++;
+            if (curTex < texAnim1.Length) StartCoroutine(Switch());
+        }
+        else if (targetAnim == 2)
+        {
+            GetComponent<Renderer>().materials[targetMat].SetTexture("_MainTex", texAnim2[curTex]);
+            curTex++;
+            if (curTex < texAnim2.Length) StartCoroutine(Switch());
+        }
+        else if (targetAnim == 3)
+        {
+            GetComponent<Renderer>().materials[targetMat].SetTexture("_MainTex", texAnim3[curTex]);
+            curTex++;
+            if (curTex < texAnim3.Length) StartCoroutine(Switch());
+        }
+        else if (targetAnim == 4)
+        {
+            GetComponent<Renderer>().materials[targetMat].SetTexture("_MainTex", texAnim4[curTex]);
+            curTex++;
+            if (curTex < texAnim4.Length) StartCoroutine(Switch());
+        }
+        else if (targetAnim == 5)
+        {
+            GetComponent<Renderer>().materials[targetMat].SetTexture("_MainTex", texAnim5[curTex]);
+            curTex++;
+            if (curTex < texAnim5.Length) StartCoroutine(Switch());
+        }
     }
 }
