f596e7dbe1ad947d1287d58a096fe7388562a263
DevGuyBlake <bmartin@dmi.edu>
2022-05-13 20:46:30 -0500

    Changed popup outlines to materials, removed pump visibility in chapterselect scene.

--- Assets/CareXR/Scripts/PictureInPicture.cs
+++ Assets/CareXR/Scripts/PictureInPicture.cs
@@ -1,42 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using BFVR.Interactable;
 
 public class PictureInPicture : MonoBehaviour
 {
     public Animator anim;
-    public BFVRHighlight highlight;
     float stepStartTime;
     float flashStartTime;
     bool showing = false;
     bool hi = false;
+    string animNameSet = "Hide";
 
     void Update()
     {
-        if (!showing && Time.time > stepStartTime+15)//make the item appear after x seconds.
+        if (!showing && Time.time > stepStartTime+15)//make the play the set animation after after 15 seconds.
         {
             showing = true;
-            anim.SetTrigger("Show");
-        }
-        if (showing && !hi && Time.time > flashStartTime+0.5f)//make the highlight flash while the item is active.
-        {
-            highlight.ShowHightlight();//this function is misspelled in the highlight script. I don't want to fix it because all the steps in all scenes would have to be updated.
-            hi = true;
-        }
-        if (showing && hi && Time.time > flashStartTime+1)
-        {
-            highlight.HideHighlight();
-            hi = false;
-            flashStartTime = Time.time;
+            anim.SetTrigger(animNameSet);
         }
     }
 
-    public void SetStepTime()
+    public void SetStepTime(string animName)//Calling this at the start of every interactable step will cause picture-in-picture to trigger the entered animation.
     {
         showing = false;
         stepStartTime = Time.time;
-        highlight.HideHighlight();
         anim.SetTrigger("Hide");
+        animNameSet = animName;
     }
 }
